547f798b4c26481c4a1e7d7e3a7bb9cd253b18e5
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-06-25 07:37:12 +0200

    prepare sources for docker webinar

--- commands/mysql-create-dangling_volume.sh
+++
@@ -1,1 +0,0 @@
-docker create --name mysql -e MYSQL_ROOT_PASSWORD=javacream -p 4306:3306 mysql:5.7.23
--- commands/nexus.sh
+++
@@ -1,3 +0,0 @@
-docker create --name nexus3 -p 9081:8081 -p 9082:8082 -p 9083:8083 -p 9084:8084 sonatype/nexus3
-
-
