1896c8e96b63d413ecd2a3a4dce334c3dd9fb84c
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-11-20 13:41:18 +0100

    add docker compose demo

--- java_projects/org.javacream.training.java.simple/src/main/java/org/javacream/training/java/simple/SimpleMain.java
+++ java_projects/org.javacream.training.java.simple/src/main/java/org/javacream/training/java/simple/SimpleMain.java
@@ -3,7 +3,7 @@ package org.javacream.training.java.simple;
 public class SimpleMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 
 }
